df0e76dea325fc4ecabbce823c26a7c39df1416d
Austen <rawleag@gmail.com>
2015-08-17 12:28:47 -0700

    input now resets to the prompt

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ form.addEventListener('submit', function(e){
   e.preventDefault();
 })
 
-
-// add an item to the list
+// add an item to the list, reset input value
 function addItem (){
+
   var input = document.querySelector('input');
   var ul = document.querySelector('ul');
   var newItem = document.createElement('li');
+
   newItem.innerHTML = input.value;
   ul.appendChild(newItem);
+  input.value = null;
 };
 
 // adds event listener to Button which allows new items to be added
